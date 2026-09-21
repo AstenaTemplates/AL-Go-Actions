aed5f8d7f4b9f2cf6750841d5a0224895f4880b6
ThierryPermentier <50203993+ThierryPermentier@users.noreply.github.com>
2026-09-21 13:43:39 +0200

    Update PipelineCleanup.ps1

--- PipelineCleanup/PipelineCleanup.ps1
+++ PipelineCleanup/PipelineCleanup.ps1
@@ -3,14 +3,15 @@ Param(
     [string] $project = "."
 )
 
-$settings = ReadSettings -project $project -baseFolder $ENV:GITHUB_WORKSPACE -workflowName "CI/CD"
-if ([bool]$settings['keepContainer']) {
-    Write-Host "keepContainer is set - leaving container $containerName in place for reuse"
-    return
-}
-
 try {
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+
+    $settings = ReadSettings -project $project -baseFolder $ENV:GITHUB_WORKSPACE -workflowName "CI/CD"
+    if ([bool]$settings['keepContainer']) {
+        Write-Host "keepContainer is set - leaving container $containerName in place for reuse"
+        return
+    }
+
     DownloadAndImportBcContainerHelper
 
     if ($project -eq ".") { $project = "" }
